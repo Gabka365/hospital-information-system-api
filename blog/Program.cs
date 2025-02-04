aeef5f6f209bb6b67196220841fb3aae9b72dd12
Потапнёв Максим <maxpotapnev2003@gmail.com>
2025-02-04 22:46:15 +0300

    Add urls

--- blog/Program.cs
+++ blog/Program.cs
@@ -1,6 +1,8 @@
+
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls(new[] { "https://0.0.0.0:5050", "http://0.0.0.0:5060" });
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/", () => "Hello World!");
 app.Run();
