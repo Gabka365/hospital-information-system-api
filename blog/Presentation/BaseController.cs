1806146b97e60b32947197481c0501eb6efe25ac
Потапнёв Максим <maxpotapnev2003@gmail.com>
2025-02-06 22:33:40 +0300

    Add react and conroller

---
+++ blog/Presentation/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace blog.Presentation
+{
+    [ApiController]
+    [Route("/")]
+    public class BaseController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string _ = "hello from index";
+            return Ok(_);
+        }
+    }
+}
--- blog/Program.cs
+++ blog/Program.cs
@@ -1,8 +1,23 @@
 
+using blog.Presentation;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls(new[] { "https://0.0.0.0:5050", "http://0.0.0.0:5060" });
+builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy =>
+        {
+            policy.WithOrigins("http://192.168.0.105:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
 
+app.UseCors("AllowReactApp");
+app.MapControllers();
 
-app.MapGet("/", () => "Hello World!");
 app.Run();
