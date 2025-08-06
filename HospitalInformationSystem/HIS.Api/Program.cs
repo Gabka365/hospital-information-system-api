5e9ff5ac9c841aca9fbbc4d1dd72d44a224e6fd0
Maksim <maxpotapnev2003@gmail.com>
2025-08-06 18:13:08 +0300

    feat!: add token generation

--- HospitalInformationSystem/HIS.Api/Program.cs
+++ HospitalInformationSystem/HIS.Api/Program.cs
@@ -1,11 +1,17 @@
 using HIS.Api.Mappers;
 using HIS.Application;
 using HIS.Application.Database;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using System.Text.Json.Serialization;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 var conf = builder.Configuration;
 builder.WebHost.UseUrls(new[] { "http://localhost:5000", "https://localhost:5050" }!);
+
 
 builder.Services.AddControllers()
     .AddJsonOptions(opts =>
@@ -13,10 +19,29 @@ builder.Services.AddControllers()
         var enumConverter = new JsonStringEnumConverter();
         opts.JsonSerializerOptions.Converters.Add(enumConverter);
     });
+
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(conf["Jwt:Key"]!)),
+        ValidateIssuerSigningKey = true,
+        ValidateLifetime = true
+    };
+});
+
+
+builder.Services.AddAuthorization();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddApplication();
 builder.Services.AddDatabase(conf["ConnectionStrings:MySqlConnectionString"]!);
+
 
 var app = builder.Build();
 
@@ -27,9 +52,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
-
 app.UseMiddleware<ValidationErrorMappingMiddleware>();
 
 var dbInitializer = app.Services.GetRequiredService<MySqlInitializer>();
---
+++ HospitalInformationSystem/HIS.Contracts/Requests/Tokens/TokenGenerationRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Contracts.Requests.Tokens
+{
+    public class TokenGenerationRequest
+    {
+        public Guid UserId { get; set; }
+
+        public string Email { get; set; }
+
+        public Dictionary<string, object> CustomClaims { get; set; } = new();
+    }
+}
