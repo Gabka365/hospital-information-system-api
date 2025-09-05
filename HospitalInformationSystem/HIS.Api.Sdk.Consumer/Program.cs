60b24c76b5c598d896f7060209e123562c6d907a
Maksim <maxpotapnev2003@gmail.com>
2025-09-05 18:28:01 +0300

    feat: add test connection through refit

--- HospitalInformationSystem/HIS.Api.Sdk.Consumer/Program.cs
+++ HospitalInformationSystem/HIS.Api.Sdk.Consumer/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HIS.Api.Sdk;
+using Refit;
+using System.Text.Json;
+
+var hisApi = RestService.For<IHisApi>("https://localhost:5050");
+
+var doctor = await hisApi.GetDoctorAsync("17ab799c-514a-485d-97d9-3363e2e62db3");
+
+Console.WriteLine(JsonSerializer.Serialize(doctor));
---
+++ HospitalInformationSystem/HIS.Api.Sdk/IHisApi.cs
@@ -0,0 +1,16 @@
+﻿using HIS.Contracts.Responses.Doctors;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Api.Sdk
+{
+    public interface IHisApi
+    {
+        [Get(ApiEndpoints.V1.Doctors.Get)]
+        Task<DoctorResponse> GetDoctorAsync(string id);
+    }
+}
