448622558f20bd0f24f5bb6d8bf818446a21df79
Maksim <maxpotapnev2003@gmail.com>
2025-09-08 17:59:25 +0300

    feat: add sdk for patients and doctors

---
+++ HospitalInformationSystem/HIS.Api.Sdk.Consumer/AuthTokenProvider.cs
@@ -0,0 +1,66 @@
+﻿using HIS.Contracts.Requests.Auth;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Api.Sdk.Consumer
+{
+    public class AuthTokenProvider
+    {
+        private readonly HttpClient _httpClient;
+        private string _cachedToken = string.Empty; 
+        private static readonly SemaphoreSlim Lock = new (1,1);
+
+        public AuthTokenProvider(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> GetTokenAsync()
+        {
+            if (!string.IsNullOrEmpty(_cachedToken))
+            {
+                var jwt = new JwtSecurityTokenHandler().ReadJwtToken(_cachedToken);
+                var expiryTimeText = jwt.Claims.Single(claim => claim.Type == "exp").Value;
+                var expiryDateTime = UnixTimeStampToDateTime(int.Parse(expiryTimeText));
+                if (expiryDateTime > DateTime.UtcNow)
+                {
+                    return _cachedToken;
+                }
+            }
+
+            await Lock.WaitAsync();
+
+            var response = await _httpClient.PostAsJsonAsync("https://localhost:5050/api/auth/login", new UserRequest
+            {
+                UserName = "admin",
+                Password = "root",
+                Email = "admin@mail.by",
+                CustomClaims = new Dictionary<string, object> 
+                { 
+                    { "admin", true }, 
+                    { "trusted_member", true } 
+                }
+            });
+
+            IEnumerable<string> cookies = response.Headers.SingleOrDefault(header => header.Key == "Set-Cookie").Value;
+
+            var newToken = cookies.First().Substring(4, cookies.First().Length - 12);
+            _cachedToken = newToken;
+            Lock.Release();
+
+            return newToken;
+        }
+
+        private DateTime UnixTimeStampToDateTime(int time)
+        {
+            var datetime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            datetime = datetime.AddSeconds(time).ToLocalTime();
+            return datetime;
+        }
+    }
+}
--- HospitalInformationSystem/HIS.Api.Sdk/IHisApi.cs
+++ HospitalInformationSystem/HIS.Api.Sdk/IHisApi.cs
@@ -1,4 +1,9 @@
-﻿using HIS.Contracts.Responses.Doctors;
+﻿using HIS.Contracts.Requests;
+using HIS.Contracts.Requests.Doctors;
+using HIS.Contracts.Requests.Patients;
+using HIS.Contracts.Responses;
+using HIS.Contracts.Responses.Doctors;
+using HIS.Contracts.Responses.Patients;
 using Refit;
 using System;
 using System.Collections.Generic;
@@ -8,9 +13,46 @@ using System.Threading.Tasks;
 
 namespace HIS.Api.Sdk
 {
+    [Headers("Authorization: Bearer")]
     public interface IHisApi
     {
+        #region Doctors
+
         [Get(ApiEndpoints.V1.Doctors.Get)]
         Task<DoctorResponse> GetDoctorAsync(string id);
+
+        [Get(ApiEndpoints.V1.Doctors.GetAll)]
+        Task<DoctorsResponse> GetDoctorsAsync(GetAllDoctorsRequest request);
+
+        [Post(ApiEndpoints.V1.Doctors.Create)]
+        Task<DoctorResponse> CreateDoctorAsync(CreateDoctorRequest request);
+
+        [Put(ApiEndpoints.V1.Doctors.Update)]
+        Task<DoctorResponse> UpdateDoctorAsync(string id, UpdateDoctorRequest request);
+
+        [Delete(ApiEndpoints.V1.Doctors.Delete)]
+        Task<bool> DeleteDoctorAsync(string id);
+
+        #endregion
+
+
+        #region Patients
+
+        [Get(ApiEndpoints.Patients.Get)]
+        Task<PatientResponse> GetPaientAsync(string id);
+
+        [Get(ApiEndpoints.Patients.GetAll)]
+        Task<PatientsResponse> GetPatientsAsync(GetAllPatientsRequest request);
+
+        [Post(ApiEndpoints.Patients.Create)]
+        Task<PatientResponse> CreatePatientAsync(CreatePatientRequest request);
+
+        [Put(ApiEndpoints.Patients.Update)]
+        Task<PatientResponse> UpdatePatientAsync(string id, UpdatePatientRequest request);
+
+        [Delete(ApiEndpoints.Patients.Delete)]
+        Task<bool> DeletePatientAsync(string id);
+
+        #endregion
     }
 }
