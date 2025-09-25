f85968353c08582f5478419fa03176515da53d78
Maksim <maxpotapnev2003@gmail.com>
2025-09-25 18:26:12 +0300

    feat: add test for get doctor

---
+++ HospitalInformationSystem/tests/HIS.Api.Tests.Integration/HIS.Api.Tests.Integration/DoctorTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Api.Tests.Integration
+{
+    public class DoctorTests
+    {
+        HttpClient _client;
+        WebApplicationFactory<Program> _factory;
+
+        public DoctorTests() 
+        { 
+            _factory = new WebApplicationFactory<Program>();
+            _client = _factory.CreateClient();
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue($"Bearer {}");
+        }
+
+        [Fact]
+        public async Task Get_ReturnsNotFound_WhenDoctorDoesNotExist()
+        {
+            var response = await _client.GetAsync($"/api/v1/doctors/{Guid.NewGuid}");
+
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+    }
+}
