550234e619c605a331585634fe07c85239bdd047
Maksim <maxpotapnev2003@gmail.com>
2025-09-26 19:06:07 +0300

    chore: add integration tests fundamentals

--- HospitalInformationSystem/tests/HIS.Api.Tests.Integration/HIS.Api.Tests.Integration/DoctorTests.cs
+++ HospitalInformationSystem/tests/HIS.Api.Tests.Integration/HIS.Api.Tests.Integration/DoctorTests.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using HIS.Api.Tests.Integration.Helpers;
+using Microsoft.AspNetCore.Mvc.Testing;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,24 +11,74 @@ using System.Threading.Tasks;
 
 namespace HIS.Api.Tests.Integration
 {
-    public class DoctorTests
+    public class DoctorTests : IAsyncLifetime, IDisposable
     {
-        HttpClient _client;
-        WebApplicationFactory<Program> _factory;
 
         public DoctorTests() 
         { 
-            _factory = new WebApplicationFactory<Program>();
-            _client = _factory.CreateClient();
-            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue($"Bearer {}");
+        
         }
 
-        [Fact]
-        public async Task Get_ReturnsNotFound_WhenDoctorDoesNotExist()
+        [Theory(Skip = "This doesnt work at the moment")]
+        [MemberData(nameof(Data))]
+        //[ClassData(typeof(ClassData))]
+        public async Task Get_ReturnsNotFound_WhenDoctorDoesNotExist(string id)
         {
-            var response = await _client.GetAsync($"/api/v1/doctors/{Guid.NewGuid}");
+            // Arrange
+            var factory = new WebApplicationFactory<Program>();
+            var client = factory.CreateClient();
+            client.DefaultRequestHeaders.Authorization = 
+                new AuthenticationHeaderValue("Bearer", AuthHelper.GenerateToken());
 
+            // Act
+            var response = await client.GetAsync($"/api/v1/doctors/{id}");
+            
+            // Assert
             Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        public Task InitializeAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task DisposeAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {   
+        
+        }
+
+        public static IEnumerable<object[]> Data { get; } = new[]
+        {
+            new[] { Guid.NewGuid().ToString() },
+            new[] { Guid.NewGuid().ToString() },
+            new[] { Guid.NewGuid().ToString() }
+        };
+
+    }
+
+
+    public class ClassData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { Guid.NewGuid().ToString() };
+            yield return new object[] { Guid.NewGuid().ToString() };
+            yield return new object[] { Guid.NewGuid().ToString() };
+        }
+
+        IEnumerator<object[]> IEnumerable<object[]>.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
